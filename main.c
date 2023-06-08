f48af6bdfd3410fa0812102e93d56076fc67b6fc
Bruno César <121142325+BrunoTiptc@users.noreply.github.com>
2023-06-07 21:52:02 -0300

    Criando um algoritmo de pilha
    Dando seguimento ao titulo pilha com uma menu simples para aprendizado

--- main.c
+++ main.c
@@ -1,18 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define tamanho 5
 
-// criando vetores dinamicos, o vetor foi preenchido usando a funcao pow que faz a potencia no elemento;
+struct tpilha {
+    int dados[tamanho];
+    int ini;
+    int fim;
+    };
+struct tpilha pilha;
+
 int main()
 {
-    int tam;
-    int *vetor;
-    printf("Escolha o tamanho do vetor \n");
-    scanf("%d", &tam);
-    vetor = (int*) malloc(sizeof (int)*tam);
-
-    for (int i = 0; i < tam; i ++) {
-    vetor[i] = pow(2,i);
-    printf("Posição %d:  %d\n", i , vetor[i]);
-    }
+    int op = 1;
+    pilha.ini = 0;
+    pilha.fim = 0;
+    while (op != 0) {
+        pilha_mostrar();
+        menu_mostrar();
+        scanf("%d", &op);
+        switch (op) {
+            case 1:
+                pilha_entrar();
+                break;
+            case 2:
+                pilha_sair();
+                break;
+                }
+            }
     return 0;
 }
+
+void pilha_mostrar() {
+    int i;
+    printf("[ ");
+    for (i=0; i<tamanho; i++) {
+        printf("%d ", pilha.dados[i]);
+        }
+    printf("] \n\n");
+}
+
+void pilha_entrar () {
+    if (pilha.fim == tamanho) {
+        printf("A pilha esta cheia impossivel empilhar mais elementos \n");
+        }
+    else {
+    printf("Digite o valor a ser empilhado:");
+    scanf("%d", &pilha.dados[pilha.fim]);
+    pilha.fim++;
+}}
+
+void pilha_sair () {
+     if (pilha.ini == pilha.fim) {
+        printf("A pilha esta vazia nao tem nada para desempilhar \n");
+        }
+    else {
+    pilha.dados[pilha.fim-1] = 0 ;
+    pilha.fim --;
+    }
+}
+
+void menu_mostrar() {
+    printf("\n Escolha uma opção: \n");
+    printf("1 - Empilhar \n");
+    printf("2 - Desimpilhar \n");
+    printf("0 - Sair \n\n");
+    }
